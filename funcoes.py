81df2ca993244777ab463e00aae4af02bd581dcb
nicolas <nicolasjpa@al.insper.edu.br>
2026-04-30 10:41:13 -0300

    ex 4 certo

--- funcoes.py
+++ funcoes.py
@@ -28,3 +28,22 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
             nova_lista.append(dados_no_estoque[i])
     
     return [dados_rolados, nova_lista]
+
+def calcula_pontos_regra_simples(dados):
+    resultado = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    
+    for d in dados:
+        if d == 1:
+            resultado[1] += 1
+        if d == 2:
+            resultado[2] += 2
+        if d == 3:
+            resultado[3] += 3
+        if d == 4:
+            resultado[4] += 4
+        if d == 5:
+            resultado[5] += 5
+        if d == 6:
+            resultado[6] += 6
+    
+    return resultado
